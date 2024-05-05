245e2f2239aef912364dbcec9b5a50fe58e898f8
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2024-05-05 07:02:43 +0300

    Added missing videos

--- 9_promisesVideos.js
+++ 9_promisesVideos.js
@@ -1,6 +1,7 @@
 /**
  * 1. Web API: https://photos.app.goo.gl/GCgrzQz12EWYWj9T7
- * 2. Promises: https://photos.app.goo.gl/GCgrzQz12EWYWj9T7
- * 3. Promises 2: https://photos.app.goo.gl/GCgrzQz12EWYWj9T7
- * 4. Proomises 3: https://photos.app.goo.gl/WfxstryjQuWM6Q2GA
+ * 2. Promises: https://photos.app.goo.gl/XT19ktx2Cf752xK88
+ * 3. Promises 2: https://photos.app.goo.gl/QMdVbQDXN9HZsxdR6
+ * 4. Proomises 3: https://photos.app.goo.gl/pvZwwK7zz5E27GJx8
+ * 5. Proomises 4: https://photos.app.goo.gl/w3sP1bf76z8N3wcq8
  */
