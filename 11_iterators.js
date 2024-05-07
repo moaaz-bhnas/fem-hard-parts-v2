e919dc5181060ddb3202a076fea47ce52aff7b69
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2024-05-07 09:05:20 +0300

    Iterators finished

--- 11_iterators.js
+++ 11_iterators.js
@@ -13,7 +13,30 @@
  * sense most of the times we're interested in accessing data one at a time
  * like a for loop
  */
-
 for (let i = 0; i < arr.length; i++) {
   doSomething();
 }
+
+/**
+ * That function needs to hold both:
+ * 1. the structure we need to iterate
+ * 2. the position (index) we're currently at
+ * 3. return the next element
+ */
+
+/** Return a function inside a function
+ * When we do that, we get so much more than just a function .. CLOSURE
+ * How the code works: https://www.canva.com/design/DAGEhZPtXkQ/0FmpTdXny66qba5_PXwd7Q/edit?utm_content=DAGEhZPtXkQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+ */
+function createFunction(array) {
+  var i = 0;
+  function inner() {
+    const element = array[i];
+    i++;
+    return element;
+  }
+  return inner;
+}
+var returnNextElement = createFunction([1, 2, 3]);
+const element1 = returnNextElement();
+const element2 = returnNextElement();
