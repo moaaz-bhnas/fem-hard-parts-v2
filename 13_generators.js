a6d17a809833217407b6ed1970e07409e2dc8fb4
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2024-05-08 09:26:26 +0300

    WIP

---
+++ 13_generators.js
@@ -0,0 +1,38 @@
+/**
+ * Instead of writing our iterators from scratch
+ * JavaScript added a built-in constructor for iterators
+ * How the following function works: https://www.canva.com/design/DAGEkXQ6OKQ/lTEZM9cbTSzk2PKZxznbWA/edit?utm_content=DAGEkXQ6OKQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+ */
+function createFlow(array) {
+  var i = 0;
+  var inner = {
+    next() {
+      const element = array[i];
+      i++;
+      return element;
+    },
+  };
+  return inner;
+}
+
+var returnNextElement = createFlow([1, 2, 3]);
+// const element1 = returnNextElement.next();
+// const element2 = returnNextElement.next();
+// const element3 = returnNextElement.next();
+
+/**
+ * JavaScript now has a built-in tool that allows us to get the flow
+ * of data, not from a collection
+ * but from "intermediate returns"
+ * Generators allow us to dynamically set what data flows to us
+ */
+function* createFlow() {
+  const num = 10;
+  const newNum = yield num;
+  yield 5 + newNum;
+  yield 6;
+}
+
+var returnNextElement = createFlow();
+const element1 = returnNextElement.next();
+const element2 = returnNextElement.next();
